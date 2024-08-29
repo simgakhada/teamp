a8c202805787c32f1e2c152df9369072a787d027
RRRR <msgpigeon@gmail.com>
2024-08-29 12:03:58 +0900

    m

---
+++ src/main/java/com/ohgiraffers/teamp/Application.java
@@ -0,0 +1,36 @@
+package main.java.com.ohgiraffers.teamp;
+
+import java.util.Scanner;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("계산기 만들기");
+        System.out.println("첫 번째 숫자");
+        int a = sc.nextInt();
+        sc.nextLine();
+        System.out.println("연산자");
+        char c = sc.next().charAt(0);
+        sc.nextLine();
+        System.out.println("두 번째 숫자");
+        int b = sc.nextInt();
+        sc.nextLine();
+
+
+        switch (c)
+        {
+            case '+':
+                break;
+            case '-':
+                break;
+            case '*':
+                break;
+            case '/':
+                break;
+            case '%':
+                break;
+        }
+    }
+}
